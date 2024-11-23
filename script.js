47539d914df11cc856f2c4792ffee92c400504eb
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-22 23:54:45 -0500

    Change grid-row to grid-column

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ const gridContainer = document.querySelector("#grid-container");
 const gridSize = 16;
 
 for (let i = 0; i < gridSize; i++) {
-    const gridRow = document.createElement("div");
-    gridRow.classList.add("grid-row");
-    gridContainer.appendChild(gridRow);
+    const gridColumn = document.createElement("div");
+    gridColumn.classList.add("grid-column");
+    gridContainer.appendChild(gridColumn);
     for (let j = 0; j < gridSize; j++) {
         const gridCell = document.createElement("div");
         gridCell.classList.add("grid-cell")
-        gridRow.appendChild(gridCell);
+        gridColumn.appendChild(gridCell);
     };
 };
 
 const gridCells = document.querySelectorAll(".grid-cell");
-const gridRow = document.querySelector(".grid-row");
+const gridColumn = document.querySelector(".grid-column");
 
 gridCells.forEach(cell => {
-    cell.style.width = `calc(90vh / ${gridSize})`
+    cell.style.height = `calc(min(100vw, 90vh) / ${gridSize})`
     let opacity = 0;
     cell.addEventListener("mouseenter", () => {
         opacity += 10;
